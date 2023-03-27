7a275fac9b334652bfe4c4b6f092b067fc468338
NadiaPeeya <nadiapeeya05@gmail.com>
2023-03-27 06:22:58 +0600

    update the server url

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ loader(messageDiv);
 
 //fetch data from server - > bot's response
 
-const response = await fetch('http://localhost:5000',{
+const response = await fetch('https://nadias-chatbot.onrender.com',{
   
   method: 'POST',
   headers: { 
